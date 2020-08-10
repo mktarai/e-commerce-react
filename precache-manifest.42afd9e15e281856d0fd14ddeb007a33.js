27e060b24735c1f15c43ad0bd4e9c46ca96effc0
Monish Tarai <monishtarai@hotmail.com>
2020-08-10 14:25:19 +0530

    Updates

---
+++ precache-manifest.42afd9e15e281856d0fd14ddeb007a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d878fea98cbc7fa6de6259075fbb57",
+    "url": "/e-commerce-react/index.html"
+  },
+  {
+    "revision": "03fcee363bf887b5031d",
+    "url": "/e-commerce-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "232340ea30f52b256e8b",
+    "url": "/e-commerce-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "03fcee363bf887b5031d",
+    "url": "/e-commerce-react/static/js/2.ea1bce43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/e-commerce-react/static/js/2.ea1bce43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232340ea30f52b256e8b",
+    "url": "/e-commerce-react/static/js/main.1e3b9820.chunk.js"
+  },
+  {
+    "revision": "53cb19f2c4af14786836",
+    "url": "/e-commerce-react/static/js/runtime-main.5f35f448.js"
+  }
+]);
